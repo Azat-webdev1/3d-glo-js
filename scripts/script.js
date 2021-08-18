7de0a0fdc9df29eeb929cde3f87a90a2bd791fb4
Azat-webdev <62510440+Azat-webdev@users.noreply.github.com>
2021-08-18 20:07:56 +0400

    feat: создал переменные

--- scripts/script.js
+++ scripts/script.js
@@ -1,57 +1,16 @@
 'use strict';
 
-window.addEventListener('DOMContentLoaded', () => {
-  const countTimer = (dedline) => {
-    let timerHours = document.querySelector('#timer-hours'),
-      timerMinutes = document.querySelector('#timer-minutes'),
-      timerSeconds = document.querySelector('#timer-seconds');
-    
-    let idInterval = 0;
-    
-    const getTimeRemaining = () => {
-      let dateStop = new Date(dedline).getTime(),
-        dateNow = new Date().getTime(),
-        timeRemaining = (dateStop - dateNow) / 1000;
-      
-      let seconds = 0,
-        minutes = 0,
-        hours = 0;
-      
-      if (timeRemaining > 0) {
-          seconds = Math.floor(timeRemaining % 60);
-          minutes = Math.floor((timeRemaining / 60) % 60);
-          hours = Math.floor(timeRemaining / 60 / 60);
-        }
-      
-      return {
-        timeRemaining,
-        hours,
-        minutes,
-        seconds
-      };
-    };
-
-    const addZero = (elem) => {
-      if (String(elem).length === 1) {
-        return '0' + elem;
-      } else {
-        return String(elem);
-      }
-    };
-
-    const updateClock = () => {
-      let timer = getTimeRemaining();
-
-      timerHours.textContent = addZero(timer.hours);
-      timerMinutes.textContent = addZero(timer.minutes);
-      timerSeconds.textContent = addZero(timer.seconds);
-
-      if (timer.timeRemaining < 0) {
-        clearInterval(idInterval);
-      } 
-    };
-    idInterval = setInterval(updateClock, 1000);
-  };
-
-  countTimer('19 Aug 2021');
-});
+const date = new Date(),
+    days = [
+        'Воскресенье',
+        'Понедельник',
+        'Вторник',
+        'Среда',
+        'Четверг',
+        'Пятница',
+        'Суббота'
+    ],
+    hours = date.getHours(),
+    day = days[date.getDay()],
+    time = date.toLocaleTimeString('ru'),
+    newDate = new Date(new Date().getFullYear() + 1, 0, 1);
