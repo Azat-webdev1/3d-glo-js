7eba24b26fd956bc411472f901dd3c25659f9711
Azat-webdev <62510440+Azat-webdev@users.noreply.github.com>
2021-08-26 18:50:28 +0400

    feat: получил элементы и создал countSum

--- scripts/script.js
+++ scripts/script.js
@@ -372,4 +372,42 @@ window.addEventListener('DOMContentLoaded', () => {
   };
   inputValid();
 
+  //Калькулятор
+  const calc = (price = 100) => {
+    const calcBlock = document.querySelector('.calc-block'),
+			calcType = document.querySelector('.calc-type'),
+			calcSquary = document.querySelector('.calc-square'),
+			calcDay = document.querySelector('.calc-day'),
+			calcCount = document.querySelector('.calc-count'),
+      totalValue = document.getElementById('total');
+    
+      const countSum = () => {
+        let total = 0,
+          counrValue = 1,
+          dayValue = 10;
+        const typeValue = calcType.options[calcType.selectedIndex].value,
+          squareValue = +calcSquary.value;
+  
+        if (calcCount.value > 1) {
+          counrValue += (calcCount.value - 1) / 10;
+        }
+  
+        if (calcDay.value) {
+          if (calcDay.value < 5) {
+            dayValue *= 2;
+          } else if (calcDay.value < 10) {
+            dayValue *= 1.5;
+          }
+        }
+  
+        if (!!typeValue && !!squareValue) {
+          total = price * typeValue * squareValue * counrValue * dayValue;
+        }
+  
+        totalValue.textContent = total;
+      };
+  
+  };
+  calc();
+
 });
