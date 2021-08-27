a6ac4b0b0ffbaf05d17d927a1ec7ef3356198241
Azat-webdev <62510440+Azat-webdev@users.noreply.github.com>
2021-08-27 14:18:18 +0400

    fix: исправил ошибку в calc

--- scripts/script.js
+++ scripts/script.js
@@ -153,7 +153,7 @@ window.addEventListener('DOMContentLoaded', () => {
       }
     };
     toggleTabContent(0);
-    
+
     tabHeader.addEventListener('click', (e) => {
       let target = e.target;
 
@@ -293,25 +293,17 @@ window.addEventListener('DOMContentLoaded', () => {
       emailInputs = document.querySelectorAll('input[placeholder="E-mail"]'),
       phoneInputs = document.querySelectorAll('input[placeholder="Номер телефона"]'),
       inputAll = document.querySelectorAll('input');
-
-    const inputCalc = () => {
-
-      const pattern = /[^\d]/g;
-      const allowedCodes = [8, 9, 27, 35, 36, 37, 38, 39, 46, 110, 188];
-
-      calc.addEventListener('input', (e) => {
-        let target = e.target;
-
-        if ((target.value.match(pattern) ||
-            allowedCodes.some(code => code === e.keyCode))) {
-          target.value = target.value.slice(0, -1);
-        }
-
-      });
-
-    };
-    inputCalc();
-
+    
+      
+      const inputCalc = () => {
+        calc.addEventListener('input', (e) => {
+          e.target.value = e.target.value.replace(/[^0-9 \.]/g, "");
+          
+        });
+      };
+      inputCalc();
+    
+    
     const inputNameText = () => {
 
       inputTexts.forEach(elem => {
@@ -357,17 +349,17 @@ window.addEventListener('DOMContentLoaded', () => {
         elem.value = text;
       }
     };
-    
+
     inputAll.forEach((elem) => {
       elem.addEventListener('blur', e => {
-        
+
         if (e.target.closest('input')) {
           checkInputAll(e.target);
         }
 
       }, true);
     });
-    
+
 
   };
   inputValid();
@@ -375,39 +367,47 @@ window.addEventListener('DOMContentLoaded', () => {
   //Калькулятор
   const calc = (price = 100) => {
     const calcBlock = document.querySelector('.calc-block'),
-			calcType = document.querySelector('.calc-type'),
-			calcSquary = document.querySelector('.calc-square'),
-			calcDay = document.querySelector('.calc-day'),
-			calcCount = document.querySelector('.calc-count'),
+      calcType = document.querySelector('.calc-type'),
+      calcSquary = document.querySelector('.calc-square'),
+      calcDay = document.querySelector('.calc-day'),
+      calcCount = document.querySelector('.calc-count'),
       totalValue = document.getElementById('total');
-    
-      const countSum = () => {
-        let total = 0,
-          counrValue = 1,
-          dayValue = 10;
-        const typeValue = calcType.options[calcType.selectedIndex].value,
-          squareValue = +calcSquary.value;
-  
-        if (calcCount.value > 1) {
-          counrValue += (calcCount.value - 1) / 10;
+
+    const countSum = () => {
+      let total = 0,
+        countValue = 1,
+        dayValue = 10;
+      const typeValue = calcType.options[calcType.selectedIndex].value,
+        squareValue = +calcSquary.value;
+
+      if (calcCount.value > 1) {
+        countValue += (calcCount.value - 1) / 10;
+      }
+
+      if (calcDay.value) {
+        if (calcDay.value < 5) {
+          dayValue *= 2;
+        } else if (calcDay.value < 10) {
+          dayValue *= 1.5;
         }
-  
-        if (calcDay.value) {
-          if (calcDay.value < 5) {
-            dayValue *= 2;
-          } else if (calcDay.value < 10) {
-            dayValue *= 1.5;
-          }
-        }
-  
-        if (!!typeValue && !!squareValue) {
-          total = price * typeValue * squareValue * counrValue * dayValue;
-        }
-  
-        totalValue.textContent = total;
-      };
-  
+      }
+
+      if (!!typeValue && !!squareValue) {
+        total = price * typeValue * squareValue * countValue * dayValue;
+      }
+
+      totalValue.textContent = total;
+    };
+
+    calcBlock.addEventListener('change', event => {
+      const target = event.target;
+      if (target.matches('.calc-day') || target.matches('.calc-type') ||
+        target.matches('.calc-square') || target.matches('.calc-count')) {
+        countSum();
+      }
+    });
+
   };
-  calc();
+  calc(100);
 
 });
